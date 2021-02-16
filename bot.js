7faa64d440adb9aa65b353f2e76fc605ac73cb04
Lisa Davies <ld443@kent.ac.uk>
2021-02-16 14:29:46 +0000

    added coffee

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+    if (query.indexOf("coffee") != -1 || query.indexOf("donuts") != -1 || query.indexOf("drink") != -1)
+    return "I would love coffee and donuts but it would destroy my circuits!";  
   return "Sorry Dave, I cannot do that."
 }
 
