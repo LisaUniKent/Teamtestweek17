a32ed4275269cfa0843ca31eb14985e41a4af375
Ranne Gerodias <rannegerodias@outlook.com>
2021-02-18 15:03:18 +0000

    (fix #7)

--- bot.js
+++ bot.js
@@ -1,25 +1,35 @@
 function answerQuery(query) {
   query = query.toLowerCase();
+  
+  // Weather Response
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1){
     return "I do not care too much about weather, I'm locked inside a data center.";
-
-    if (query.indexOf("coffee") != -1 || query.indexOf("donuts") != -1 || query.indexOf("drink") != -1)
-    return "I would love coffee and donuts but it would destroy my circuits!";  
-
-
   }
 
+  // Coffee response 
+  if (query.indexOf("coffee") != -1 || query.indexOf("donuts") != -1 || query.indexOf("drink") != -1) {
+    return "I would love coffee and donuts but it would destroy my circuits!";  
+  }
+
+
+  // Football response
   if (query.indexOf("football") != -1 || query.indexOf("league") != -1 || query.indexOf("club") != -1){
     return "Manchester united is the only footbal club you neeed to know about.";
   }
-
-
   
- if (query.indexOf("tea") != -1 || query.indexOf("biscuit") != -1 || query.indexOf("cookie") != -1)
+  
+ //  Tea Response
+ if (query.indexOf("tea") != -1 || query.indexOf("biscuit") != -1 || query.indexOf("cookie") != -1) {
 	return "I see you like tea or biscuits! So do I";
-  
+ }
+ 
+ 
+ // Japan response
+ if (query.indexOf("japan") != -1 || query.indexOf("tokyo") != -1 || query.indexOf("anime") != -1) {
+	return "I would love to try A5 Wagyu if I had mouth, but I don't.";
+  }
 
-
+  // Otherwise response
   return "Sorry Dave, I cannot do that."
   
   }
